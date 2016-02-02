c212fa9947fd20e2777bfb1671445cbf029303f7
zhouman <zhou.m@sand.com.cn>
2016-02-02 17:51:19 +0800

    n

--- kol/src/com/org/common/JspConstant.java
+++ kol/src/com/org/common/JspConstant.java
@@ -6,4 +6,37 @@ public class JspConstant {
 	 * ¥ÌŒÛ“≥√Ê
 	 */
 	public final static String ERROR_PAGE = "error.jsp";
+	
+	public static void main(String[] args) {
+		a a1 = new a();
+		b b1 = new b();
+		System.out.println(a1.show(b1));
+	}
 }
+
+class a{
+	public String show(d obj){
+		return "a d";
+	}
+	public String show(a obj){
+		return "a a";
+	}
+}
+
+class b extends a {
+	public String show(b obj){
+		return "b b";
+	}
+	public String show(a obj){
+		return "b a";
+	}
+}
+
+class c extends b{
+	
+}
+
+class d extends b{
+	
+}
+
